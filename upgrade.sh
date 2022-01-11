844464642989b2cb22134bc9d3450e16566a2670
Adam Jedlička <adajedlicka@gmail.com>
2022-01-11 15:37:52 +0100

    Update

--- upgrade.sh
+++ upgrade.sh
@@ -2,12 +2,24 @@
 
 set -e
 
+echo "=> stopping the application"
+
 docker-compose down
+
+echo "=> pulling new version"
 
 git pull
 
+echo "=> removing old cache"
+
 rm -rf temp/cache
+
+echo "=> starting new version"
 
 docker-compose up --build -d
 
+echo "=> migrating database"
+
 docker-compose exec app php vendor/bin/phinx migrate
+
+echo "=> done"
