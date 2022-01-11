36abe3b52d3af6fa9162c3b644f9c8ae88f08007
Adam Jedlička <adajedlicka@gmail.com>
2022-01-11 15:39:41 +0100

    Fix build

--- upgrade.sh
+++ upgrade.sh
@@ -2,24 +2,12 @@
 
 set -e
 
-echo "=> stopping the application"
-
 docker-compose down
-
-echo "=> pulling new version"
 
 git pull
 
-echo "=> removing old cache"
-
 rm -rf temp/cache
-
-echo "=> starting new version"
 
 docker-compose up --build -d
 
-echo "=> migrating database"
-
-docker-compose exec app php vendor/bin/phinx migrate
-
-echo "=> done"
+docker-compose exec -T app php vendor/bin/phinx migrate
